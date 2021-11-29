aa13688aa6016e8bb60a14d4c44b23b0a2e507a9
Eric Stokes <letaris@gmail.com>
2021-11-29 00:56:33 -0500

    excel called a function in my interface

--- src/interface.rs
+++ src/interface.rs
@@ -1,5 +1,11 @@
 use com::{interfaces::IUnknown, sys::{HRESULT, IID}};
-use winapi::um::oaidl::{SAFEARRAY, VARIANT};
+use winapi::{
+    shared::{minwindef::{WORD, UINT}, wtypesbase::LPOLESTR},
+    um::{
+        oaidl::{SAFEARRAY, VARIANT, ITypeInfo, DISPID, DISPPARAMS, EXCEPINFO}, 
+        winnt::LCID
+    }, 
+};
 
 // bde5f32a-14d9-414e-a0af-8390a1601944
 pub const CLSID: IID = IID {
@@ -10,15 +16,40 @@ pub const CLSID: IID = IID {
 };
 
 com::interfaces! {
+    #[uuid("00020400-0000-0000-C000-000000000046")]
+    pub unsafe interface IDispatch: IUnknown {
+        pub fn get_type_info_count(&self, info: *mut UINT) -> HRESULT;
+        pub fn get_type_info(&self, lcid: LCID, type_info: *mut *mut ITypeInfo) -> HRESULT;
+        pub fn get_ids_of_names(
+            &self, 
+            riid: *const IID, 
+            names: *const LPOLESTR, 
+            names_len: UINT, 
+            lcid: LCID, 
+            ids: *mut DISPID
+        ) -> HRESULT;
+        pub fn invoke(
+            &self, 
+            id: DISPID, 
+            iid: *const IID, 
+            lcid: LCID, 
+            flags: WORD, 
+            params: *mut DISPPARAMS,
+            result: *mut VARIANT,
+            exception: *mut EXCEPINFO,
+            arg_error: *mut UINT
+        ) -> HRESULT;
+    }
+
     #[uuid("A43788C1-D91B-11D3-8F39-00C04F3651B8")]
-    pub unsafe interface IRTDUpdateEvent: IUnknown {
+    pub unsafe interface IRTDUpdateEvent: IDispatch{
         pub fn update_notify(&self) -> HRESULT;
         pub fn heartbeat_interval(&self, hb: *mut i32) -> HRESULT;
         pub fn disconnect(&self) -> HRESULT;
     }
 
     #[uuid("EC0E6191-DB51-11D3-8F3E-00C04F3651B8")]
-    pub unsafe interface IRTDServer: IUnknown {
+    pub unsafe interface IRTDServer: IDispatch {
         pub fn server_start(&self, cb: *const IRTDUpdateEvent, res: *mut i32) -> HRESULT;
         pub fn connect_data(
             &self, 
--- src/server.rs
+++ src/server.rs
@@ -1,13 +1,49 @@
-use crate::interface::{IRTDServer, IRTDUpdateEvent};
-use winapi::um::oaidl::{SAFEARRAY, VARIANT};
-use com::sys::{HRESULT, NOERROR};
+use crate::interface::{IDispatch, IRTDServer, IRTDUpdateEvent};
+use winapi::{
+    shared::{minwindef::{WORD, UINT}, wtypesbase::LPOLESTR},
+    um::{
+        oaidl::{SAFEARRAY, VARIANT, ITypeInfo, DISPID, DISPPARAMS, EXCEPINFO}, 
+        winnt::LCID
+    }, 
+};
+use com::{sys::{HRESULT, NOERROR, IID}};
 
 com::class! {
     #[derive(Debug)]
-    pub class NetidxRTD: IRTDServer {}
+    pub class NetidxRTD: IRTDServer(IDispatch) {}
+
+    impl IDispatch for NetidxRTD {
+        fn get_type_info_count(&self, _info: *mut UINT) -> HRESULT { NOERROR }
+        fn get_type_info(&self, _lcid: LCID, _type_info: *mut *mut ITypeInfo) -> HRESULT { NOERROR }
+
+        pub fn get_ids_of_names(
+            &self, 
+            _riid: *const IID, 
+            _names: *const LPOLESTR, 
+            _names_len: UINT, 
+            _lcid: LCID, 
+            _ids: *mut DISPID
+        ) -> HRESULT {
+            std::fs::write("C:\\Users\\eric\\proj\\netidx-excel\\log.txt", "ids of names called").unwrap();
+            NOERROR
+        }
+
+        fn invoke(
+            &self, 
+            _id: DISPID, 
+            _iid: *const IID, 
+            _lcid: LCID, 
+            _flags: WORD, 
+            _params: *mut DISPPARAMS,
+            _result: *mut VARIANT,
+            _exception: *mut EXCEPINFO,
+            _arg_error: *mut UINT
+        ) -> HRESULT { NOERROR }
+    }
 
     impl IRTDServer for NetidxRTD {
         fn server_start(&self, _cb: *const IRTDUpdateEvent, _res: *mut i32) -> HRESULT {
+            std::fs::write("C:\\Users\\eric\\proj\\netidx-excel\\log.txt", "I was initialized").unwrap();
             NOERROR
         }
 
